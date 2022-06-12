b4b04c848bd7a4069a03951beb5ef9d6d0de1e89
Cristian Díaz <cristian.diaz@monstar-lab.com>
2022-06-12 13:26:19 +0200

    Copy resources

--- Package.swift
+++ Package.swift
@@ -23,7 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CustomAnnotatedMap",
-            dependencies: []),
+            dependencies: [],
+            resources: [.copy("Resources")]
+        ),
         .testTarget(
             name: "CustomAnnotatedMapTests",
             dependencies: ["CustomAnnotatedMap"]),
