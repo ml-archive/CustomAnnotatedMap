e83a4e7c5c027625d73b095975f2b92e51052612
BucekJiri <jibu@nodesagency.com>
2022-07-28 13:59:42 +0200

    Helpers

--- Sources/CustomAnnotatedMap/Helpers/MapKit.swift
+++ Sources/CustomAnnotatedMap/Helpers/MapKit.swift
@@ -1,6 +1,6 @@
 //
-//  File.swift
-//  
+//  MapKit.swift
+//
 //
 //  Created by Jiří Buček on 28.07.2022.
 //
@@ -8,17 +8,29 @@
 import Foundation
 import MapKit
 
-extension MKCoordinateRegion {
-    func equals(to other: MKCoordinateRegion) -> Bool {
-        center.equals(to: other.center)
-        && span.equals(to: other.span)
-    }
-}
-
 extension CLLocationCoordinate2D {
+
     func equals(to other: CLLocationCoordinate2D) -> Bool {
         latitude == other.latitude
         && longitude == other.longitude
     }
+
 }
 
+extension MKCoordinateRegion {
+
+    func equals(to other: MKCoordinateRegion) -> Bool {
+        center.equals(to: other.center)
+        && span.equals(to: other.span)
+    }
+    
+}
+
+extension MKCoordinateSpan {
+
+    func equals(to other: MKCoordinateSpan) -> Bool {
+        latitudeDelta == other.latitudeDelta
+        && longitudeDelta == other.longitudeDelta
+    }
+
+}
